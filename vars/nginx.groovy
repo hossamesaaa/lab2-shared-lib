6bf2dceedf1d126869db49620f5f7df5f221ea17
hossamesaaa <87980797+hossamesaaa@users.noreply.github.com>
2023-09-22 17:12:31 +0300

    Update nginx.groovy

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -1,34 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(){
-   
-
-   pipeline {
-    
-    agent {
-      label  'slave1'
-    }
-
-    stages {
-        
-        stage('install nginx') {
-            steps {
-               sh 'sudo yum install nginx -y'
-            }
-        }
-        stage('start nginx') {
-            steps {
-                sh "sudo systemctl enable nginx "
-                 sh "sudo systemctl start nginx "
-            }    
-        }
-       stage('test nginx') {
-            steps {
-                sh "curl localhost "
-            }    
-        }
-       
-    }
-  
-}
+def call(String name = 'human') {
+  echo "Hello, ${name}."
 }
