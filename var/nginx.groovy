9340556668bcdb266f140bb163a9e658a6252c7c
hossamesaaa <87980797+hossamesaaa@users.noreply.github.com>
2023-09-22 12:10:46 +0300

    Update nginx.groovy

--- var/nginx.groovy
+++ var/nginx.groovy
@@ -1,9 +1,30 @@
 def call(){
 
+   pipeline {
+    
+    agent {
+      label: slave1
+    }
 
-
-
-
-
+    stages {
+        
+        stage('install nginx') {
+            steps {
+               sh 'sudo yum install nginx'
+            }
+        }
+        stage('start nginx') {
+            steps {
+                sh "sudo systemctl enable nginx "
+                 sh "sudo systemctl start nginx "
+            }    
+        }
+       stage('test nginx') {
+            steps {
+                sh "curl localhost "
+            }    
+        }
+       
+    }
   
 }
